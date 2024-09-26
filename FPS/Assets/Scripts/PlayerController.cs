c86b1ee8a16f0d4db5ca089d545d96b9f31b90e1
nothinghere1990 <john1990xup6@gmail.com>
2024-09-26 16:37:47 +0800

    Add sprint and jump gravity.

--- FPS/Assets/Scripts/PlayerController.cs
+++ FPS/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,32 +10,43 @@ public class PlayerController : MonoBehaviour
 
     public bool invertLook;
 
-    public float moveSpeed = 5;
+    public float moveSpeed = 5, runSpeed = 8;
+    public float activeMoveSpeed;
     private Vector3 moveDir, movement;
 
     public CharacterController charCtrl; //include rigidbody and collision
+
+    private Camera cam;
+
+    public float jumpForce = 12, gravityMod = 2.5f;
+
+    public Transform groundCheckPoint;
+    public bool isGrounded;
+    public LayerMask groundLayers;
     
     void Start()
     {
-        //Lock mouse cursor position to center of the game screen.
+        //Lock and hide mouse cursor position to center of the game screen.
         Cursor.lockState = CursorLockMode.Locked;
         
         charCtrl = GetComponent<CharacterController>();
+        
+        cam = Camera.main;
     }
     
     void Update()
     {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
         
-        //horizontal rotation
+        //Horizontal Rotation
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
-        //vertical rotation
+        //Vertical Rotation
         verticalRotStore += mouseInput.y;
         
         // Limit look up and down angle.
         verticalRotStore = Mathf.Clamp(verticalRotStore, -60, 60);
         
-        // invert control
+        // Invert Control
         if (invertLook)
         {
             viewPoint.rotation = Quaternion.Euler(verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
@@ -44,13 +56,61 @@ public class PlayerController : MonoBehaviour
             viewPoint.rotation = Quaternion.Euler(-verticalRotStore, viewPoint.rotation.eulerAngles.y, viewPoint.rotation.eulerAngles.z);
         }
 
-        // move input
+        // Move Input
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        // Move vector length change to 1.
-        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized;
-
-        charCtrl.Move(movement * moveSpeed * Time.deltaTime);
         
-        transform.position += movement * moveSpeed * Time.deltaTime;
+        // Running
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            activeMoveSpeed = runSpeed;
+        }
+        else
+        {
+            activeMoveSpeed = moveSpeed;
+        }
+
+        float yVel = movement.y;
+        
+        // Transform move vector length change to 1.
+        movement = ((transform.forward * moveDir.z) + (transform.right * moveDir.x)).normalized * activeMoveSpeed;
+
+        movement.y = yVel;
+
+        if (charCtrl.isGrounded)
+        {
+            movement.y = 0;
+        }
+        
+        // Ground Raycast
+        isGrounded = Physics.Raycast(groundCheckPoint.position, Vector3.down, .5f, groundLayers);
+        Debug.DrawRay(groundCheckPoint.position, Vector3.down * .5f, Color.green);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("jump");
+            movement.y = jumpForce;
+        }
+
+        movement.y += Physics.gravity.y * Time.deltaTime * gravityMod;
+        
+        charCtrl.Move(movement * Time.deltaTime);
+        
+        // Show cursor.
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    // Camera binds to view point.
+    private void LateUpdate()
+    {
+        cam.transform.position = viewPoint.position;
+        cam.transform.rotation = viewPoint.rotation;
     }
 }
